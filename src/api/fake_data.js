d01de7f030ddf7d347fed29f24ecd948a9b120de
michbat <michael.batn@hotmail.com>
2025-12-22 22:29:54 +0100

    Implement fetchRecommendations API Call

--- src/api/fake_data.js
+++ src/api/fake_data.js
@@ -16,3 +16,458 @@ export const FAKE_POPULARS = [
         vote_count: 19626
     }
 ];
+
+
+export const FAKE_RECOMMENDATIONS = [
+    {
+        adult: false,
+        backdrop_path: "/yGNnjoIGOdQy3douq60tULY8teK.jpg",
+        id: 63247,
+        name: "Westworld",
+        original_name: "Westworld",
+        overview: "A dark odyssey about the dawn of artificial consciousness and the evolution of sin. Set at the intersection of the near future and the reimagined past, it explores a world in which every human appetite, no matter how noble or depraved, can be indulged.",
+        poster_path: "/8MfgyFHf7XEboZJPZXCIDqqiz6e.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            10765,
+            37
+        ],
+        popularity: 29.5984,
+        first_air_date: "2016-10-02",
+        vote_average: 8.028,
+        vote_count: 5934,
+        origin_country: [
+            "US"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/dg3OindVAGZBjlT3xYKqIAdukPL.jpg",
+        id: 42009,
+        name: "Black Mirror",
+        original_name: "Black Mirror",
+        overview: "Twisted tales run wild in this mind-bending anthology series that reveals humanity's worst traits, greatest innovations and more.",
+        poster_path: "/seN6rRfN0I6n8iDXjlSMk1QjNcq.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            10765,
+            18,
+            9648
+        ],
+        popularity: 39.1217,
+        first_air_date: "2011-12-04",
+        vote_average: 8.288,
+        vote_count: 5852,
+        origin_country: [
+            "GB"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/gHJhN9AJV4PmJ7YpLFa9ldDWuG8.jpg",
+        id: 75006,
+        name: "The Umbrella Academy",
+        original_name: "The Umbrella Academy",
+        overview: "A dysfunctional family of superheroes comes together to solve the mystery of their father's death, the threat of the apocalypse and more.",
+        poster_path: "/qhcwrnnCnN8NE1N6XXKHFmveJR9.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            10759,
+            10765,
+            18
+        ],
+        popularity: 14.8428,
+        first_air_date: "2019-02-15",
+        vote_average: 8.498,
+        vote_count: 9810,
+        origin_country: [
+            "US"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/7cqKGQMnNabzOpi7qaIgZvQ7NGV.jpg",
+        id: 76479,
+        name: "The Boys",
+        original_name: "The Boys",
+        overview: "A group of vigilantes known informally as “The Boys” set out to take down corrupt superheroes with no more than blue-collar grit and a willingness to fight dirty.",
+        poster_path: "/2zmTngn1tYC1AvfnrFLhxeD82hz.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            10765,
+            10759
+        ],
+        popularity: 58.923,
+        first_air_date: "2019-07-25",
+        vote_average: 8.446,
+        vote_count: 11450,
+        origin_country: [
+            "US"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/tsRy63Mu5cu8etL1X7ZLyf7UP1M.jpg",
+        id: 1396,
+        name: "Breaking Bad",
+        original_name: "Breaking Bad",
+        overview: "Walter White, a New Mexico chemistry teacher, is diagnosed with Stage III cancer and given a prognosis of only two years left to live. He becomes filled with a sense of fearlessness and an unrelenting desire to secure his family's financial future at any cost as he enters the dangerous world of drugs and crime.",
+        poster_path: "/ztkUQFLlC19CCMYHW9o1zWhJRNq.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            18,
+            80
+        ],
+        popularity: 153.8286,
+        first_air_date: "2008-01-20",
+        vote_average: 8.936,
+        vote_count: 16720,
+        origin_country: [
+            "US"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/nfWhNM5VVd9a5bEGobs83TWTuLM.jpg",
+        id: 74577,
+        name: "The End of the F***ing World",
+        original_name: "The End of the F***ing World",
+        overview: "James is 17 and is pretty sure he is a psychopath. Alyssa, also 17, is the cool and moody new girl at school. The pair make a connection and she persuades him to embark on a darkly comedic road trip in search of her real father.",
+        poster_path: "/f1OV9xEJCZVYcYSDRr5xOD8NJw3.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            35,
+            18,
+            80
+        ],
+        popularity: 8.2649,
+        first_air_date: "2017-10-24",
+        vote_average: 8.044,
+        vote_count: 3432,
+        origin_country: [
+            "GB"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/g9ju2o4LioYYOyihBvf9lVX8XL.jpg",
+        id: 38472,
+        name: "Marvel's Jessica Jones",
+        original_name: "Marvel's Jessica Jones",
+        overview: "After a tragic ending to her short-lived super hero stint, Jessica Jones is rebuilding her personal life and career as a detective who gets pulled into cases involving people with extraordinary abilities in New York City.",
+        poster_path: "/paf9wL3mOW9LT3ZjRxXqJcjeMEv.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            10765,
+            18
+        ],
+        popularity: 9.0852,
+        first_air_date: "2015-11-20",
+        vote_average: 7.481,
+        vote_count: 2501,
+        origin_country: [
+            "US"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/8rvLEmdI4gLrMO1rLqbNdnNcPFE.jpg",
+        id: 19885,
+        name: "Sherlock",
+        original_name: "Sherlock",
+        overview: "A modern update finds the famous sleuth and his doctor partner solving crime in 21st century London.",
+        poster_path: "/7WTsnHkbA0FaG6R9twfFde0I9hl.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            80,
+            18,
+            9648
+        ],
+        popularity: 25.0669,
+        first_air_date: "2010-07-25",
+        vote_average: 8.517,
+        vote_count: 6075,
+        origin_country: [
+            "GB"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/yUOFocKDW7MCC5isx4FK8A68QFp.jpg",
+        id: 4607,
+        name: "Lost",
+        original_name: "Lost",
+        overview: "Stripped of everything, the survivors of a horrific plane crash  must work together to stay alive. But the island holds many secrets.",
+        poster_path: "/og6S0aTZU6YUJAbqxeKjCa3kY1E.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            9648,
+            10759,
+            18
+        ],
+        popularity: 102.6603,
+        first_air_date: "2004-09-22",
+        vote_average: 7.947,
+        vote_count: 4799,
+        origin_country: [
+            "US"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/d77WPPNbuvyGJJ675UFokVmOKHb.jpg",
+        id: 90260,
+        name: "I Am Not Okay with This",
+        original_name: "I Am Not Okay with This",
+        overview: "Angsty Syd navigates high school awkwardness, family drama and an unrequited crush on her best friend while trying to rein in her budding superpowers.",
+        poster_path: "/kf3yX0ILNlLJ42X3lX2iYJ3QRp6.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            18,
+            35,
+            10765
+        ],
+        popularity: 5.7103,
+        first_air_date: "2020-02-26",
+        vote_average: 8.01,
+        vote_count: 1901,
+        origin_country: [
+            "US"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/c4CSgKL6QfkJxsWcGYDyTxpbzpW.jpg",
+        id: 66788,
+        name: "13 Reasons Why",
+        original_name: "13 Reasons Why",
+        overview: "After a teenage girl's perplexing suicide, a classmate receives a series of tapes that unravel the mystery of her tragic choice.",
+        poster_path: "/nel144y4dIOdFFid6twN5mAX9Yd.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            18,
+            9648
+        ],
+        popularity: 22.28,
+        first_air_date: "2017-03-31",
+        vote_average: 7.562,
+        vote_count: 4416,
+        origin_country: [
+            "US"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/zZqpAXxVSBtxV9qPBcscfXBcL2w.jpg",
+        id: 1399,
+        name: "Game of Thrones",
+        original_name: "Game of Thrones",
+        overview: "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
+        poster_path: "/1XS1oqL89opfnbLl8WnZY1O1uJx.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            10765,
+            18,
+            10759
+        ],
+        popularity: 105.8669,
+        first_air_date: "2011-04-17",
+        vote_average: 8.46,
+        vote_count: 25944,
+        origin_country: [
+            "US"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/75HgaphatW0PDI3XIHQWZUpbhn6.jpg",
+        id: 70523,
+        name: "Dark",
+        original_name: "Dark",
+        overview: "A missing child sets four families on a frantic hunt for answers as they unearth a mind-bending mystery that spans three generations.",
+        poster_path: "/apbrbWs8M9lyOpJYU5WXrpFbk1Z.jpg",
+        media_type: "tv",
+        original_language: "de",
+        genre_ids: [
+            80,
+            18,
+            10765,
+            9648
+        ],
+        popularity: 26.5068,
+        first_air_date: "2017-12-01",
+        vote_average: 8.421,
+        vote_count: 7258,
+        origin_country: [
+            "DE"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/kSIQWeamejVQAW5LCfgLAS8ZWv9.jpg",
+        id: 1424,
+        name: "Orange Is the New Black",
+        original_name: "Orange Is the New Black",
+        overview: "A crime she committed in her youthful past sends Piper Chapman to a women's prison, where she trades her comfortable New York life for one of unexpected camaraderie and conflict in an eccentric group of fellow inmates.",
+        poster_path: "/ekaa7YjGPTkFLcPhwWXTnARuCEU.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            35,
+            18
+        ],
+        popularity: 21.0373,
+        first_air_date: "2013-07-11",
+        vote_average: 7.654,
+        vote_count: 2708,
+        origin_country: [
+            "US"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/xGexTKCJDkl12dTW4YCBDXWb1AD.jpg",
+        id: 71446,
+        name: "Money Heist",
+        original_name: "La casa de papel",
+        overview: "To carry out the biggest heist in history, a mysterious man called The Professor recruits a band of eight robbers who have a single characteristic: none of them has anything to lose. Five months of seclusion - memorizing every step, every detail, every probability - culminate in eleven days locked up in the National Coinage and Stamp Factory of Spain, surrounded by police forces and with dozens of hostages in their power, to find out whether their suicide wager will lead to everything or nothing.",
+        poster_path: "/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg",
+        media_type: "tv",
+        original_language: "es",
+        genre_ids: [
+            80,
+            18
+        ],
+        popularity: 29.0642,
+        first_air_date: "2017-05-02",
+        vote_average: 8.233,
+        vote_count: 19179,
+        origin_country: [
+            "ES"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/9In9QgVJx7PlFOAgVHCKKSbo605.jpg",
+        id: 60625,
+        name: "Rick and Morty",
+        original_name: "Rick and Morty",
+        overview: "Rick is a mentally-unbalanced but scientifically gifted old man who has recently reconnected with his family. He spends most of his time involving his young grandson Morty in dangerous, outlandish adventures throughout space and alternate universes. Compounded with Morty's already unstable family life, these events cause Morty much distress at home and school.",
+        poster_path: "/WGRQ8FpjkDTzivQJ43t94bOuY0.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            16,
+            35,
+            10765,
+            10759
+        ],
+        popularity: 55.3297,
+        first_air_date: "2013-12-02",
+        vote_average: 8.7,
+        vote_count: 10553,
+        origin_country: [
+            "US"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/mrGd5M3kPkAKynZruOAwsadsr9w.jpg",
+        id: 69050,
+        name: "Riverdale",
+        original_name: "Riverdale",
+        overview: "Set in the present, the series offers a bold, subversive take on Archie, Betty, Veronica and their friends, exploring the surreality of small-town life, the darkness and weirdness bubbling beneath Riverdale’s wholesome facade.",
+        poster_path: "/d8mmn9thQ5dBk2qbv6BCqGUXWK3.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            80,
+            18,
+            9648
+        ],
+        popularity: 31.0703,
+        first_air_date: "2017-01-26",
+        vote_average: 8.419,
+        vote_count: 13633,
+        origin_country: [
+            "US"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/3URK0z9PzpVNJrGE7XOuyy6KFzk.jpg",
+        id: 87108,
+        name: "Chernobyl",
+        original_name: "Chernobyl",
+        overview: "The true story of one of the worst man-made catastrophes in history: the catastrophic nuclear accident at Chernobyl. A tale of the brave men and women who sacrificed to save Europe from unimaginable disaster.",
+        poster_path: "/hlLXt2tOPT6RRnjiUmoxyG1LTFi.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            18
+        ],
+        popularity: 21.802,
+        first_air_date: "2019-05-06",
+        vote_average: 8.694,
+        vote_count: 7351,
+        origin_country: [
+            "US"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/a906PH7CDmSOdS7kmnAgdWk5mhv.jpg",
+        id: 67744,
+        name: "MINDHUNTER",
+        original_name: "MINDHUNTER",
+        overview: "An agent in the FBI's Elite Serial Crime Unit develops profiling techniques as he pursues notorious serial killers and rapists.",
+        poster_path: "/fbKE87mojpIETWepSbD5Qt741fp.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            18,
+            80
+        ],
+        popularity: 12.2396,
+        first_air_date: "2017-10-13",
+        vote_average: 8.099,
+        vote_count: 2780,
+        origin_country: [
+            "US"
+        ]
+    },
+    {
+        adult: false,
+        backdrop_path: "/hZOuVkhAWX9viLJuaIsMh8cM3Jz.jpg",
+        id: 62560,
+        name: "Mr. Robot",
+        original_name: "Mr. Robot",
+        overview: "A contemporary and culturally resonant drama about a young programmer, Elliot, who suffers from a debilitating anti-social disorder and decides that he can only connect to people by hacking them. He wields his skills as a weapon to protect the people that he cares about. Elliot will find himself in the intersection between a cybersecurity firm he works for and the underworld organizations that are recruiting him to bring down corporate America.",
+        poster_path: "/kv1nRqgebSsREnd7vdC2pSGjpLo.jpg",
+        media_type: "tv",
+        original_language: "en",
+        genre_ids: [
+            80,
+            18
+        ],
+        popularity: 19.2415,
+        first_air_date: "2015-06-24",
+        vote_average: 8.259,
+        vote_count: 5086,
+        origin_country: [
+            "US"
+        ]
+    }
+];
--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,5 +1,5 @@
 // import axios from "axios";
-import { FAKE_POPULARS } from "./fake_data";
+import { FAKE_POPULARS, FAKE_RECOMMENDATIONS } from "./fake_data";
 import { BASE_URL, API_KEY_PARAM } from "../config";
 
 
@@ -11,5 +11,14 @@ export class TVShowAPI {
         // return response.data.results;
         // console.log(FAKE_POPULARS)
         return FAKE_POPULARS;
+    }
+    static async fetchRecommendations(tvShowId) {
+        const i = tvShowId;
+        console.log(i)
+        // const response = await axios.get(`${BASE_URL}tv/${tvShowId}/recommendations${API_KEY_PARAM}`);
+        // console.log(response.data.results);
+        // return response.data.results;
+        return FAKE_RECOMMENDATIONS;
+
     }
 }
