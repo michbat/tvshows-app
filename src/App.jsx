55ddfcbbe789379772e32110dcb4175a82676bc7
michbat <michael.batn@hotmail.com>
2025-12-22 14:16:17 +0100

    Setting fonts

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ export const App = ()=> {
 
   return (
     <>
+    <div className="container">
+     <h1>HELLO</h1>
+     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ipsam quam sit, cupiditate rerum dolorem veniam necessitatibus quo, ipsum deserunt commodi, architecto minus? Harum repellendus, ex voluptates culpa non qui est obcaecati suscipit iste voluptatem earum nostrum quod molestiae fuga voluptas adipisci cupiditate at repudiandae.</p>
+    </div>
      
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,15 @@
+import "@fontsource/roboto/100-italic.css";
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/300-italic.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400-italic.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500-italic.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700-italic.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/900-italic.css";
+import "@fontsource/roboto/900.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
