fbd8750e8c41d58500f719c49a78f6f4853bad06
michbat <michael.batn@hotmail.com>
2025-12-22 18:57:29 +0100

    Hidding API Key, rendering data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { TVShowAPI } from "./api/tv-show";
+import { BACKDROP_BASE_URL } from "./config";
 
 export const App = () => {
+  const [currentTVShow, setCurrentTVShow] = useState();
+
+  useEffect(() => {
+    const getPopulars = async () => {
+      const popularTVShowList = await TVShowAPI.fetchPopulars();
+
+      if (popularTVShowList.length > 0) {
+        setCurrentTVShow(popularTVShowList[0]);
+      }
+    };
+
+    getPopulars();
+  }, []);
+
+  console.log(currentTVShow);
+
   return (
     <>
-      <div className="main_container">
+      <div
+        className="main_container"
+        style={{
+          background: currentTVShow
+            ? `linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)),
+             url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover`
+            : "#333",
+        }}
+      >
         <div className="header">
           <div className="row">
             <div className="col-4">
@@ -11,7 +38,7 @@ export const App = () => {
               <div>Subtitle</div>
             </div>
             <div className="col-md-12 col-lg-4">
-              <input style={{width:"100%"}} type="text" />
+              <input style={{ width: "100%" }} type="text" />
             </div>
           </div>
         </div>
---
+++ src/api/fake_data.js
@@ -0,0 +1,18 @@
+export const FAKE_POPULARS = [
+    {
+        adult: false,
+        backdrop_path: "/8zbAoryWbtH0DKdev8abFAjdufy.jpg",
+        first_air_date: "2016-07-15",
+        genre_ids: [10765, 9648, 10759],
+        id: 66732,
+        name: "Stranger Things",
+        origin_country: ["US"],
+        original_language: "en",
+        original_name: "Stranger Things",
+        overview: "When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces, and one strange little girl.",
+        popularity: 526.3515,
+        poster_path: "/cVxVGwHce6xnW8UaVUggaPXbmoE.jpg",
+        vote_average: 8.597,
+        vote_count: 19626
+    }
+];
